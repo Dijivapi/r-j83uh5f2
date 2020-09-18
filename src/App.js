aa070214be42c957c24d80afe4899d586f8e0be5
Diana <dianitavan@gmail.com>
2020-09-17 20:14:41 -0500

    cambio en app.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
-      </div>
-    );
-  }
+    state = {
+        counter: "0"
+    }
+    change = (event) => {
+        console.log(event.target.value)
+        var oldCount = this.state.counter;
+        oldCount += event.target.value
+        console.log(oldCount);
+        const newCount = oldCount.length - 1;
+        console.log(newCount)
+        this.setState({ counter: newCount });
+    }
+
+    render() {
+        return ( <
+            div className = "container" >
+            <
+            textarea rows = "3"
+            onChange = { this.change } > < /textarea> <
+            div className = "counter" > { this.state.counter } < /div> < /
+            div >
+        );
+    }
 }
 
 export default App;
